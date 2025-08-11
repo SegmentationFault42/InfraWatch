3dfa6d807c4ae1e5d83b5c3b340fc03cb9cb2cf4
nyingikawatela <lausiowatela@gmail.com>
2025-08-11 09:57:52 +0100

    fix: Resolução de bugs na estrutura

--- infraWatch/prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    semi: true, // ponto e vírgula no final
-    singleQuote: true, // aspas simples
-    trailingComma: 'all', // vírgula no último item
-    tabWidth: 4, // 2 espaços por tab
-    printWidth: 80, // quebra linha a cada 80 caracteres
-};
--- infraWatch/src/api/routes/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import statusRoutes from './status.routes.js';
-
-export function Routes(app: FastifyInstance) {
-    app.register(statusRoutes);
-}
